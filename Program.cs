f4176e64cd912a5b29d511955130cbadcd268bac
Bryan Boettcher <bryan.boettcher@gmail.com>
2024-07-24 12:34:31 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using nietras.SeparatedValues;
+
+namespace POC;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var reader = Sep.Reader( o => o with
+        {
+            ColNameComparer = StringComparer.OrdinalIgnoreCase,
+            CultureInfo = CultureInfo.InvariantCulture,
+            Unescape = true,
+            DisableQuotesParsing = false,
+            DisableColCountCheck = true
+        } ).FromFile( "broken.csv" );
+
+        foreach ( var row in reader )
+        {
+        }
+    }
+}
